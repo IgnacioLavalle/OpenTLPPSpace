976e60a1a44e144ecd01bfcc9c194c476c8be88c
Nacho <ignacio.lavalle.cobo@gmail.com>
2025-07-08 11:45:42 -0300

    Agrego prints de metrica por snapshot a GCNGAN

--- Python/GCN_GAN_SMP.py
+++ Python/GCN_GAN_SMP.py
@@ -51,6 +51,16 @@ def append_classification_metrics_with(c0precision_list, c0recall_list, c0f1_lis
     c0f1_list.append(f1_per_class[0])
     c1f1_list.append(f1_per_class[1])
 
+    print('  C0 Prec: %f C0 Rec: %f  C0 F1: %f' %
+        (precision_per_class[0],
+        recall_per_class[0],
+        f1_per_class[0]))
+    print('  C1 Prec: %f  C1 Rec: %f  C1 F1: %f ' %
+        (precision_per_class[1],
+        recall_per_class[1], 
+        f1_per_class[1]))
+    print()
+
 
 def mean_and_std_from_classlists(c0_list, c1_list):
     c0_mean = np.mean(c0_list)
@@ -387,11 +397,15 @@ def main():
         true_labels = (true_vals >= 1).astype(int)
         pred_labels = (pred_vals >= 1).astype(int)
 
+        print('Test snapshot %d metrics per snapshot:'
+            % (t))
+        print()
+
         append_classification_metrics_with(c0precision_list, c0recall_list, c0f1_list, c1precision_list, c1recall_list, c1f1_list, true_labels, pred_labels)
 
         #Errors
         abs_errors = np.abs(pred_vals - true_vals)
-        sq_errors = (pred_vals - true_vals) ** 2
+        sq_errors = (pred_vals - true_vals) ** 2 
 
         #MAE and std
         filtered_mae = np.mean(abs_errors)
@@ -401,6 +415,11 @@ def main():
 
         RMSE = filtered_rmse
         MAE = filtered_mae
+
+        print('RMSE %f MAE %f'
+            % (RMSE, MAE))
+        print()
+
         # ==========
         RMSE_list.append(RMSE)
         MAE_list.append(MAE)
